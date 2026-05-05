346090d3b11bf4d837778f8fc9be938ee73cab6b
Lukas Erni <le@kraftausdruck.ch>
2026-05-05 18:36:07 +0200

    Fix PHPUnit 11 config and line length linting error

--- tests/SelectUploadFieldTestForm.php
+++ tests/SelectUploadFieldTestForm.php
@@ -37,7 +37,10 @@ class SelectUploadFieldTestForm extends Form implements TestOnly
     public function getRecord()
     {
         if (empty($this->record)) {
-            $this->record = DataObject::get(SelectUploadFieldTestRecord::class, ['Title' => 'Record1'])->setUseCache(true)->first();
+            $this->record = DataObject::get(SelectUploadFieldTestRecord::class)
+                ->filter(['Title' => 'Record1'])
+                ->setUseCache(true)
+                ->first();
         }
         return $this->record;
     }
